98e69413b289a45e0598fcea8223c5029bae3232
Florian Knodt <git@adlerweb.info>
2016-11-17 10:14:25 +0100

    gluon-config-mode-autoupdater

--- site.mk
+++ site.mk
@@ -12,6 +12,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location-ffmyk \
 	gluon-config-mode-contact-info \
+	gluon-config-mode-autoupdater \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
